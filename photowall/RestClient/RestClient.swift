925095eb27d65030b1f1ab980132cdf775764ce8
Chien-Fu <student@studentde-iMac-166.local>
2017-05-12 17:30:09 +0800

    201705121729
    201705121729

---
+++ photowall/RestClient/RestClient.swift
@@ -0,0 +1,15 @@
+//
+//  RestClient.swift
+//  photowall
+//
+//  Created by student on 2017/5/12.
+//  Copyright © 2017年 chienfu. All rights reserved.
+//
+
+import UIKit
+
+class RestClient: NSObject {
+
+    //var path:RestRequest
+    
+}
---
+++ photowall/RestClient/RestRequest.swift
@@ -0,0 +1,14 @@
+//
+//  RestRequest.swift
+//  photowall
+//
+//  Created by student on 2017/5/12.
+//  Copyright © 2017年 chienfu. All rights reserved.
+//
+
+import UIKit
+import AFNetworking
+
+protocol RestRequest: NSObjectProtocol {
+
+}
